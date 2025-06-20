86b6a2b3157356c9d805edd55c79806db8f17849
Mattbrn <114392135+Mattbrn0@users.noreply.github.com>
2025-06-20 15:02:49 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+    'http://localhost:5173',
+    'https://bubblehouse-frontend.vercel.app'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
